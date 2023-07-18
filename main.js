60bc3372192b8e430abd01edb41af94e89ee44b2
quang2204 <quangltph37691@fpt.edu.vn>
2023-07-18 17:10:56 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(".image-slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  infinite: true,
+  prevArrow: `<button type='button' class='slick-prev slick-arrow'><i class="bi bi-arrow-left-square-fill"></i></button>`,
+  nextArrow: `<button type='button' class='slick-next slick-arrow'><i class="bi bi-arrow-right-square-fill"></i></button>`,
+});
